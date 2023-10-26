2006cd3bf39fd38d5e260612968be189edb3fb2f
someshjagdhane <mrjagdhane007@gmail.com>
2023-10-26 21:27:39 +0530

    my-work carousel updated

--- js/index.js
+++ js/index.js
@@ -25,57 +25,57 @@ function closeMenu(){
 
 // --------- My Work ----------------
 
-// let currentProj = 1;
-// const totalProj = allProjects.length;
+let currentProj = 1;
+const totalProj = allProjects.length;
 
-// function goToProject(projectNum){
-//     allProjects.forEach((project, i)=>{
-//         project.style.transform = `translateX(${100*(i-projectNum)}%)`;
-//     }); 
-// }
-// function nextProj(){
-//     let project =currentProj;
-//     project++;
-//     return project = project > totalProj-1 ? 0 : project;
-// }
-// function prevProj(){
-//     let project = currentProj;
-//     project--;
-//     return project = project < 0 ? totalProj-1 : project;
-// }
-// function switchProj(proj){
-//     allProjects.forEach(project=> project.classList.remove(`active-project`));
-//     document.querySelector(`.project-${proj}`).classList.add(`active-project`);
-// }
-// function switchDot(dotNum){
-//     allDots.forEach(dot=> dot.classList.remove(`active-dot`));
-//     document.getElementById(`dot-${dotNum}`).classList.add(`active-dot`);
-// }
-// function changeProject(project){
-//     goToProject(project);
-//     switchProj(project);
-//     switchDot(project);
-// }
-// btnRight.addEventListener(`click`,function(){
-//     currentProj = nextProj();
-//     changeProject(currentProj);
-// });
+function goToProject(projectNum){
+    allProjects.forEach((project, i)=>{
+        project.style.transform = `translateX(${100*(i-projectNum)}%)`;
+    }); 
+}
+function nextProj(){
+    let project =currentProj;
+    project++;
+    return project = project > totalProj-1 ? 0 : project;
+}
+function prevProj(){
+    let project = currentProj;
+    project--;
+    return project = project < 0 ? totalProj-1 : project;
+}
+function switchProj(proj){
+    allProjects.forEach(project=> project.classList.remove(`active-project`));
+    document.querySelector(`.project-${proj}`).classList.add(`active-project`);
+}
+function switchDot(dotNum){
+    allDots.forEach(dot=> dot.classList.remove(`active-dot`));
+    document.getElementById(`dot-${dotNum}`).classList.add(`active-dot`);
+}
+function changeProject(project){
+    goToProject(project);
+    switchProj(project);
+    switchDot(project);
+}
+btnRight.addEventListener(`click`,function(){
+    currentProj = nextProj();
+    changeProject(currentProj);
+});
 
-// btnLeft.addEventListener(`click`,function(){
-//     currentProj=prevProj();
-//     changeProject(currentProj);
+btnLeft.addEventListener(`click`,function(){
+    currentProj=prevProj();
+    changeProject(currentProj);
    
-// });
+});
 
-// dotContainer.addEventListener(`click`,function(e){
-//     const targetDot = e.target;
-//     if(!targetDot.classList.contains(`indicator-dot`)) return;
-//     const project = targetDot.dataset.dot;
-//     currentProj=project;
-//     changeProject(project);
-// })
+dotContainer.addEventListener(`click`,function(e){
+    const targetDot = e.target;
+    if(!targetDot.classList.contains(`indicator-dot`)) return;
+    const project = targetDot.dataset.dot;
+    currentProj=project;
+    changeProject(project);
+})
 
-// changeProject(currentProj);
+changeProject(currentProj);
 
 let themeLight = false;
 themeBtn.addEventListener(`click`,e=>{
